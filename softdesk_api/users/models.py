07e485e0f9b527986a1af3ed344c4c466d53dfb0
MaximeLanca <lanca.maxime.ber@gmail.com>
2025-07-14 21:59:22 +0200

    delete users with same email

--- softdesk_api/users/models.py
+++ softdesk_api/users/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     age = models.PositiveIntegerField(null=True, blank=True)
+    email = models.EmailField(unique=True)
 
     def __str__(self):
         return self.username
