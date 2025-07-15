3d8ccbc9beec3a438b8012b23f2d1b6630b25b3e
MaximeLanca <lanca.maxime.ber@gmail.com>
2025-07-15 12:55:15 +0200

    Create project model

--- softdesk_api/projects/models.py
+++ softdesk_api/projects/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django import settings
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length = 128)
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, related_name = "users")
+    time_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
